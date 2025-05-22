488f3cb7a3da3db19db667a59d79e63f05e2b633
Gaurav Sinha <gaurav.sinha1@materialplus.io>
2025-05-22 11:49:22 +0530

    first commit

---
+++ src/main/java/com/budgetpulse/microservices/ClientUserManagementServiceApplication.java
@@ -0,0 +1,11 @@
+package com.budgetpulse.microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientUserManagementServiceApplication {
+		public static void main(String[] args) {
+			System.out.println("HELLO");
+		}
+	}
